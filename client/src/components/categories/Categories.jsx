a0650dd2457e931f370a51b42203780d41540085
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-07-27 22:27:41 +0700

    update

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,54 +1,55 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { request } from '../../utils/fetchApi'
-import { format } from 'timeago.js'
-import { Link } from 'react-router-dom'
-import classes from './categories.module.css'
-import { MdOutlinePreview } from 'react-icons/md'
-import { AiFillLike } from 'react-icons/ai'
-import { FiArrowRight } from 'react-icons/fi'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { request } from "../../utils/fetchApi";
+import { format } from "timeago.js";
+import { Link } from "react-router-dom";
+import classes from "./categories.module.css";
+import { MdOutlinePreview } from "react-icons/md";
+import { AiFillLike } from "react-icons/ai";
+import { FiArrowRight } from "react-icons/fi";
 
 const Categories = () => {
-  const [blogs, setBlogs] = useState([])
-  const [filteredBlogs, setFilteredBlogs] = useState([])
-  const [activeCategory, setActiveCategory] = useState('all')
+  const [blogs, setBlogs] = useState([]);
+  const [filteredBlogs, setFilteredBlogs] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("all");
   const categories = [
-    'all',
-    'nature',
-    'music',
-    'travel',
-    'design',
-    'programming',
-    'fun',
-    'fashion'
-  ]
+    "all",
+    "nature",
+    "music",
+    "travel",
+    "design",
+    "programming",
+    "fun",
+    "fashion",
+  ];
 
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const data = await request('/blog/getAll', 'GET')
-        setBlogs(data)
-        setFilteredBlogs(data)
+        const data = await request("/blog/getAll", "GET");
+        setBlogs(data);
+        setFilteredBlogs(data);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
-    fetchBlogs()
-  }, [])
+    };
+    fetchBlogs();
+  }, []);
 
   useEffect(() => {
-    if(activeCategory === 'all'){
-      setFilteredBlogs(blogs)
+    if (activeCategory === "all") {
+      setFilteredBlogs(blogs);
     } else {
       setFilteredBlogs((prev) => {
-        const filteredBlogs = blogs.filter((blog) => blog.category.toLowerCase() === activeCategory.toLowerCase())
+        const filteredBlogs = blogs.filter(
+          (blog) => blog.category.toLowerCase() === activeCategory.toLowerCase()
+        );
 
-        return filteredBlogs
-      })
+        return filteredBlogs;
+      });
     }
-  }, [activeCategory])
-
+  }, [activeCategory]);
 
   return (
     <div className={classes.container}>
@@ -59,19 +60,23 @@ const Categories = () => {
             {categories.map((category) => (
               <span
                 key={crypto.randomUUID()}
-                className={`${classes.category} ${activeCategory === category && classes.active}`}
-                onClick={() => setActiveCategory(prev => category)}
+                className={`${classes.category} ${
+                  activeCategory === category && classes.active
+                }`}
+                onClick={() => setActiveCategory((prev) => category)}
               >
                 {category}
               </span>
             ))}
           </div>
-          {filteredBlogs?.length > 0 ?
+          {filteredBlogs?.length > 0 ? (
             <div className={classes.blogs}>
               {filteredBlogs?.map((blog) => (
                 <div key={blog._id} className={classes.blog}>
                   <Link to={`/blogDetails/${blog?._id}`}>
-                    <img src={`http://localhost:5000/images/${blog?.photo}`} />
+                    <img
+                      src={`https://backend-blogit.onrender.com/images/${blog?.photo}`}
+                    />
                   </Link>
                   <div className={classes.blogData}>
                     <div className={classes.categoryAndMetadata}>
@@ -84,25 +89,32 @@ const Categories = () => {
                       </div>
                     </div>
                     <h4>{blog?.title}</h4>
-                    <p className={classes.blogDesc}>
-                      {blog?.desc}
-                    </p>
+                    <p className={classes.blogDesc}>{blog?.desc}</p>
                     <div className={classes.authorAndCreatedAt}>
-                      <span><span>Author:</span> {blog?.userId?.username}</span>
-                      <span><span>Created:</span> {format(blog?.createdAt)}</span>
+                      <span>
+                        <span>Author:</span> {blog?.userId?.username}
+                      </span>
+                      <span>
+                        <span>Created:</span> {format(blog?.createdAt)}
+                      </span>
                     </div>
-                    <Link to={`/blogDetails/${blog._id}`} className={classes.readMore}>
+                    <Link
+                      to={`/blogDetails/${blog._id}`}
+                      className={classes.readMore}
+                    >
                       Read More <FiArrowRight />
                     </Link>
                   </div>
                 </div>
               ))}
             </div>
-            : <h3 className={classes.noBlogsMessage}>No blogs</h3>}
+          ) : (
+            <h3 className={classes.noBlogsMessage}>No blogs</h3>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
--- client/src/pages/create/Create.jsx
+++ client/src/pages/create/Create.jsx
@@ -1,79 +1,77 @@
-import React from 'react'
-import { useState } from 'react'
-import Footer from '../../components/footer/Footer'
-import Navbar from '../../components/navbar/Navbar'
-import classes from './create.module.css'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { AiOutlineCloseCircle } from 'react-icons/ai'
-import { request } from '../../utils/fetchApi'
+import React from "react";
+import { useState } from "react";
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+import classes from "./create.module.css";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { request } from "../../utils/fetchApi";
 
 const Create = () => {
-  const [title, setTitle] = useState("")
-  const [desc, setDesc] = useState("")
-  const [img, setImg] = useState("")
-  const [category, setCategory] = useState("")
-  const navigate = useNavigate()
-  const { token } = useSelector((state) => state.auth)
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [img, setImg] = useState("");
+  const [category, setCategory] = useState("");
+  const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth);
 
   const categories = [
-    'nature',
-    'music',
-    'travel',
-    'design',
-    'programming',
-    'fun',
-    'fashion'
-  ]
+    "nature",
+    "music",
+    "travel",
+    "design",
+    "programming",
+    "fun",
+    "fashion",
+  ];
 
   const onChangeFile = (e) => {
-    setImg(e.target.files[0])
-  }
+    setImg(e.target.files[0]);
+  };
 
   const handleCloseImg = () => {
-    setImg(null)
-  }
+    setImg(null);
+  };
 
   const handleCreateBlog = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      const formData = new FormData()
+      const formData = new FormData();
 
-      let filename = null
+      let filename = null;
       if (img) {
-        filename = crypto.randomUUID() + img.name
-        formData.append("filename", filename)
-        formData.append("image", img)
+        filename = crypto.randomUUID() + img.name;
+        formData.append("filename", filename);
+        formData.append("image", img);
 
-        await fetch(`http://localhost:5000/upload`, {
+        await fetch(`https://backend-blogit.onrender.com/upload`, {
           method: "POST",
-          body: formData
-        })
+          body: formData,
+        });
       } else {
-        return
+        return;
       }
 
       const options = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      }
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      };
 
       const body = {
         title,
         desc,
         category,
-        photo: filename
-      }
+        photo: filename,
+      };
 
-
-      const data = await request('/blog', "POST", options, body)
-      navigate(`/blogDetails/${data._id}`)
-
+      const data = await request("/blog", "POST", options, body);
+      navigate(`/blogDetails/${data._id}`);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
     <>
@@ -86,7 +84,7 @@ const Create = () => {
               <label>Title: </label>
               <input
                 type="text"
-                placeholder='Title...'
+                placeholder="Title..."
                 className={classes.input}
                 onChange={(e) => setTitle(e.target.value)}
               />
@@ -95,21 +93,26 @@ const Create = () => {
               <label>Description: </label>
               <input
                 type="text"
-                placeholder='Description...'
+                placeholder="Description..."
                 className={classes.input}
                 onChange={(e) => setDesc(e.target.value)}
               />
             </div>
             <div className={classes.inputWrapperSelect}>
               <label>Category: </label>
-              <select value={category} onChange={(e) => setCategory(e.target.value)}>
+              <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
                 {categories.map((category) => (
-                  <option key={crypto.randomUUID()} value={category}>{category}</option>
+                  <option key={crypto.randomUUID()} value={category}>
+                    {category}
+                  </option>
                 ))}
               </select>
             </div>
             <div className={classes.inputWrapperImg}>
-              <label htmlFor='image' className={classes.labelFileInput}>
+              <label htmlFor="image" className={classes.labelFileInput}>
                 Image: <span>Upload here</span>
               </label>
               <input
@@ -117,9 +120,17 @@ const Create = () => {
                 type="file"
                 className={classes.input}
                 onChange={onChangeFile}
-                style={{ display: 'none' }}
+                style={{ display: "none" }}
               />
-              {img && <p className={classes.imageName}>{img.name} <AiOutlineCloseCircle className={classes.closeIcon} onClick={() => handleCloseImg()} /></p>}
+              {img && (
+                <p className={classes.imageName}>
+                  {img.name}{" "}
+                  <AiOutlineCloseCircle
+                    className={classes.closeIcon}
+                    onClick={() => handleCloseImg()}
+                  />
+                </p>
+              )}
             </div>
             <div className={classes.buttonWrapper}>
               <button className={classes.submitBtn} type="submit">
@@ -131,7 +142,7 @@ const Create = () => {
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
