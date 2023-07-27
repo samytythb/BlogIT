4973629a272194ae5f31f0d59807550b69a0de3a
Le Minh Duc <79952214+samytythb@users.noreply.github.com>
2023-07-27 22:39:59 +0700

    update

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import classes from './footer.module.css'
+import React from "react";
+import classes from "./footer.module.css";
 
 const Footer = () => {
   return (
@@ -7,16 +7,18 @@ const Footer = () => {
       <div className={classes.wrapper}>
         <div className={classes.col}>
           <h2>About the App</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Possimus quo voluptatum, ullam quam perspiciatis deleniti obcaecati architecto,
-            sed minus culpa autem suscipit rem vero voluptas alias animi. Iure, eaque dicta!
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            quo voluptatum, ullam quam perspiciatis deleniti obcaecati
+            architecto, sed minus culpa autem suscipit rem vero voluptas alias
+            animi. Iure, eaque dicta!
           </p>
         </div>
         <div className={classes.col}>
           <h2>Contacts</h2>
           <span>Phone: +123 456 789</span>
-          <span>YouTube: WebDevMania</span>
-          <span>GitHub: WebDevMania</span>
+          <span>YouTube: Nothing </span>
+          <span>GitHub: Samyththb</span>
         </div>
         <div className={classes.col}>
           <h2>Location</h2>
@@ -26,7 +28,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
